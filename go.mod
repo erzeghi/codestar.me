7a17cbc50c36807f4808d8564e3731690a08f259
Bojan Erceg <erzeghi@gmail.com>
2022-09-11 21:51:34 +0200

    update function to do what it needs

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module aws/codestar/projecttemplates/main
 go 1.12
 
 require (
-github.com/aws/aws-lambda-go v1.17.0
-github.com/stretchr/testify v1.6.1
+	github.com/aws/aws-lambda-go v1.17.0
+	github.com/aws/aws-sdk-go v1.44.95
+	github.com/stretchr/testify v1.6.1
 )
--- main.go
+++ main.go
@@ -1,31 +1,176 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"io/ioutil"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
-// Handler is executed by AWS Lambda in the main function. Once the request
-// is processed, it returns an Amazon API Gateway response object to AWS Lambda
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+const (
+	baseURL       = "http://codestar.me/"
+	tableName     = "codestar.me"
+	maxBodyLength = 500
+)
 
-	index, err := ioutil.ReadFile("public/index.html")
+var dynoClient *dynamodb.DynamoDB
+
+type item struct {
+	Ref    string `json:"ref,omitempty"`
+	Body   string `json:"body,omitempty"`
+	Expire int64  `json:"expire,omitempty"`
+}
+
+func main() {
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	dynoClient = dynamodb.New(sess)
+
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+
+	fmt.Printf("%v", request)
+	method := request.RequestContext.HTTP.Method
+	path := request.RequestContext.HTTP.Path
+	switch method {
+	case "GET":
+		// if homepage. TODO: change integration -> route to S3
+		if path == "/" {
+			return index()
+		}
+
+		// getting single item by ref
+		i, err := getItem(refFromPath(path))
+		if err != nil {
+			return errorResponse(err, "error getting item")
+		}
+		return validResponse(i.Body)
+
+	case "POST":
+		// take body
+
+		body := strings.TrimSpace(request.Body)
+		if len(body) > maxBodyLength {
+			return errorResponse(fmt.Errorf("body limit error"), fmt.Sprintf("body too big. max %d characters", maxBodyLength))
+		}
+		ref := makeRef(body)
+
+		err := saveItem(ref, body)
+		if err != nil {
+			return errorResponse(err, "error saving item")
+		}
+
+		return validResponse(baseURL + ref)
+	default:
+		return errorResponse(fmt.Errorf("not valid method "), "not valid method")
+	}
+}
+
+func makeRef(body string) string {
+	hash := sha1.New()
+	hash.Write([]byte(body))
+	return base64.URLEncoding.EncodeToString(hash.Sum(nil))[0:5]
+}
+
+// taking only first 5 characters from the path
+func refFromPath(path string) string {
+	return path[1:6]
+}
+
+func index() (events.APIGatewayV2HTTPResponse, error) {
+	homepage, err := ioutil.ReadFile("public/index.html")
 	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+		return events.APIGatewayV2HTTPResponse{}, err
 	}
 
-	return events.APIGatewayProxyResponse{
+	return events.APIGatewayV2HTTPResponse{
 		StatusCode: 200,
-		Body:       string(index),
+		Body:       string(homepage),
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
 	}, nil
-
 }
 
-func main() {
-	lambda.Start(Handler)
+func saveItem(ref, body string) error {
+	i := item{
+		Ref:    ref,
+		Body:   body,
+		Expire: time.Now().Add(5 * time.Minute).Unix(), // expire after 5 minutes
+	}
+
+	marshaled, err := dynamodbattribute.MarshalMap(i)
+	if err != nil {
+		return fmt.Errorf("error marshalling new item: %w", err)
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      marshaled,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = dynoClient.PutItem(input)
+	if err != nil {
+		return fmt.Errorf("error calling PutItem: %w", err)
+	}
+	return nil
+}
+
+func getItem(ref string) (*item, error) {
+
+	result, err := dynoClient.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"ref": {
+				S: aws.String(ref),
+			},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error calling GetItem: %w", err)
+	}
+
+	item := new(item)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling item: %w", err)
+	}
+	return item, nil
+}
+
+func errorResponse(err error, userMessage string) (events.APIGatewayV2HTTPResponse, error) {
+	log.Println(err)
+	return events.APIGatewayV2HTTPResponse{
+		StatusCode: 500,
+		Body:       userMessage,
+		Headers: map[string]string{
+			"Content-Type": "text/plain",
+		},
+	}, nil
+}
+
+func validResponse(body string) (events.APIGatewayV2HTTPResponse, error) {
+	return events.APIGatewayV2HTTPResponse{
+		StatusCode: 200,
+		Body:       body,
+		Headers: map[string]string{
+			"Content-Type": "text/plain",
+		},
+	}, nil
 }
